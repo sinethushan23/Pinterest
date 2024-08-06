f3fd40c384a767a66a4d277afa3de0c00f85af36
Sineth Ushan <sinethushan1253@gmail.com>
2024-08-07 01:55:25 +0530

    created profile pin gallery

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -15,54 +15,82 @@ class profile extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(children: [
-        Center(
-          child: Column(children: [
-            Image.asset("lib/images/sarath-caters-high-resolution-logo.png"),
-            Align(
-              alignment: Alignment.topCenter,
-              child: CircleAvatar(
-                radius: 50,
-                backgroundImage:
-                    AssetImage('lib/images/IMG-20220827-WA0178.jpg'),
-              ),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Center(
+                  child: Column(children: [
+                    Image.asset(
+                        "lib/images/sarath-caters-high-resolution-logo.png"),
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundImage:
+                            AssetImage('lib/images/IMG-20220827-WA0178.jpg'),
+                      ),
+                    ),
+                  ]),
+                ),
+                Text(
+                  "Sineth Ushan",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                  ),
+                ),
+                Text(
+                  "Sineth_ushan",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  "Welcome!",
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Center(
+                        child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/createpin');
+                            },
+                            child: Text("Create Pin")),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                    height: 20), // space between the button and the gallery
+              ],
             ),
-          ]),
-        ),
-        Text(
-          "Sineth Ushan",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 28,
           ),
-        ),
-        Text(
-          "Sineth_ushan",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-          ),
-        ),
-        Text(
-          "Welcome!",
-          style: TextStyle(
-            fontSize: 15,
-          ),
-        ),
-        Row(
-          children: [
-            Expanded(
-              child: Center(
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/createpin');
-                    },
-                    child: Text("Create Pin")),
-              ),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: 1,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
             ),
-          ],
-        )
-      ]),
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Image.asset(
+                  'lib/images/sarath-caters-high-resolution-logo.png', //  image path
+                  fit: BoxFit.cover,
+                );
+              },
+              childCount: 99, //  number of images
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
