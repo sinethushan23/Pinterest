76fbb8796a9c56477380c65ca56f07b5015dc076
Sineth Ushan <sinethushan1253@gmail.com>
2024-08-11 00:25:34 +0530

    create account changes

--- lib/pages/createaccount.dart
+++ lib/pages/createaccount.dart
@@ -5,59 +5,99 @@ class createAccount extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      
-      appBar: AppBar( centerTitle: true,
-        title: Text('Create a new account',
-      style: TextStyle(color: const Color.fromRGBO(230, 0, 35,1
-      ),
-      ),
-      ),
-
+    final _emailController = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Create a new account',
+          style: TextStyle(
+            color: const Color.fromRGBO(230, 0, 35, 1),
+          ),
+        ),
       ),
       //Other Body parts
-      body: 
-      Padding(padding: const EdgeInsets.all(15.0),
-      child: Form(
-          //Form
-          child: Column(
-            children: 
-             [
-              
-              TextFormField(
-              decoration: InputDecoration(
-                labelText: "Enter your gmail",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: "Enter your password",
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            //elevated buttons
-
-            Row(
+      body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Form(
+            //Form
+            child: Column(
               children: [
-                
-            SizedBox(width: 120, height: 20,),
-            ElevatedButton(
-              child: Text("Create account"),
-              onPressed: (){
-                Navigator.pushNamed(context, '/home');
-              },
-              
+                SizedBox(height: 10),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Enter your name",
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Enter your nick name",
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your nick name';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: "Enter your email",
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Enter your password",
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
 
-            )
-            ],
-            )
-            ],
-          ),)
-          
-      ),
+                //elevated buttons
+
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 120,
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      child: Text("Create account"),
+                      onPressed: () async {
+                        final email = _emailController.text;
+                        final subject = 'Welcome to Pinterest!';
+                        final body = 'Confirm your email account';
+                      },
+                    )
+                  ],
+                )
+              ],
+            ),
+          )),
     );
   }
 }
